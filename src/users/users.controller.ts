0d1e88b7655dfd2db84e616d8ab57fe1357cd011
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-09-24 09:54:44 +0100

    add: get user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,15 +10,17 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUserParamDto } from './dtos/get-user-param.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUserParamDto: GetUserParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
+    console.log(getUserParamDto);
     console.log(limit);
     console.log(page);
     return 'This is the users controller';
