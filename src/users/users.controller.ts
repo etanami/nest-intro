71edfc39980ec6b259d37291617bcdbed9aecfd8
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-09-24 09:42:10 +0100

    add: create user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,32 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Ip,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param() param: any, @Query() limit: any) {
-    console.log(param);
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(limit);
+    console.log(page);
     return 'This is the users controller';
   }
 
   @Post()
-  public createUser(@Body() request: any, @Ip() ip: any) {
-    console.log(request);
+  public createUser(@Body() createUserDto: CreateUserDto, @Ip() ip: any) {
+    console.log(createUserDto);
     console.log(ip);
     return 'User created successfully';
   }
