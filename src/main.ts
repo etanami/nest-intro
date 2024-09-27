591ca3523c31f67aeb849f967d0d6063e3cf813e
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-09-27 19:09:03 +0100

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use global validation pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +14,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger configuration
+  const config = new DocumentBuilder().setVersion('1.0').build();
+
+  // Instantiate document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
