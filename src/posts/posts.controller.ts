c4b77e2165b519638e64db33abab49a03464db49
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-04 16:10:47 +0100

    add: patch post dto

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from 'src/posts/dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -16,5 +17,10 @@ export class PostsController {
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
+  }
+
+  @Patch()
+  public patchPost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
