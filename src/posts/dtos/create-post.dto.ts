30cc6561dda47fadfd6813412da7ca25ff30c227
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-04 12:20:16 +0100

    add: create post dto

---
+++ src/posts/dtos/create-post.dto.ts
@@ -0,0 +1,68 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { postType } from '../enums/postType.enum';
+import { postStatus } from '../enums/postStatus.enum';
+import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
+import { Type } from 'class-transformer';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  title: string;
+
+  @IsEnum(postType)
+  @IsNotEmpty()
+  postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
+  slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
+  status: postStatus;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsOptional()
+  @IsJSON()
+  schema?: string;
+
+  @IsOptional()
+  @IsUrl()
+  featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  @MinLength(3, { each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePostMetaOptionsDto)
+  metaOptions: CreatePostMetaOptionsDto[];
+}
