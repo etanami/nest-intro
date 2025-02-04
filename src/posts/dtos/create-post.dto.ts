50040fbf6d1cfd2030c7a34d0b06f811066b8f8f
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-04 13:31:55 +0100

    add: swagger documentation for createPostDto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -15,17 +15,21 @@ import { postType } from '../enums/postType.enum';
 import { postStatus } from '../enums/postStatus.enum';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MinLength(4)
   title: string;
 
+  @ApiProperty()
   @IsEnum(postType)
   @IsNotEmpty()
   postType: postType;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -34,35 +38,42 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty()
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsUrl()
   featuredImageUrl?: string;
 
+  @ApiPropertyOptional()
   @IsISO8601()
   @IsOptional()
   publishOn?: Date;
 
+  @ApiPropertyOptional()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   @MinLength(3, { each: true })
   tags?: string[];
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
-  metaOptions: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
