97190d1e14dd3563e549f74a9bf5afd52808c92d
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-05 19:57:48 +0100

    fix: dto matches post entity validation

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -8,6 +8,7 @@ import {
   IsString,
   IsUrl,
   Matches,
+  MaxLength,
   MinLength,
   ValidateNested,
 } from 'class-validator';
@@ -24,6 +25,7 @@ export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(4)
+  @MaxLength(512)
   title: string;
 
   /** Type of post (post/page/story/series) */
@@ -36,6 +38,7 @@ export class CreatePostDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(256)
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
     message:
       'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
@@ -64,6 +67,7 @@ export class CreatePostDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsUrl()
+  @MaxLength(1024)
   featuredImageUrl?: string;
 
   /** Scheduled publication date */
