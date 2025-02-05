850d47035c5d9717b9b0b5441b46741fbb32662f
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-05 19:54:56 +0100

    add: post entity

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,69 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { postType } from './enums/postType.enum';
+import { postStatus } from './enums/postStatus.enum';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    type: 'enum',
+    enum: postType,
+    default: postType.POST,
+    nullable: false,
+  })
+  postType: postType;
+
+  @Column({
+    type: 'varchar',
+    length: 256,
+    unique: true,
+    nullable: false,
+  })
+  slug: string;
+
+  @Column({
+    type: 'enum',
+    enum: postStatus,
+    default: postStatus.DRAFT,
+    nullable: false,
+  })
+  status: postStatus;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  content?: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  schema?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 1024,
+    nullable: true,
+  })
+  featuredImageUrl?: string;
+
+  @Column({
+    type: 'timestamp',
+    nullable: true,
+  })
+  publicOn?: string;
+
+  // worked on later
+  tags?: string;
+  metaOptions?: string;
+}
