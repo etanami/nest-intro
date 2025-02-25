313fc64bae3ca71263524fa11ff89bc892efb155
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-02-25 13:26:03 +0100

    add: exception handling for createManyUsers

--- src/users/providers/create-many-users-provider.ts
+++ src/users/providers/create-many-users-provider.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { User } from '../user.entity';
 import { DataSource } from 'typeorm';
 import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
@@ -16,10 +20,14 @@ export class CreateManyUsersProvider {
     // create query runner instance
     const queryRunner = this.dataSource.createQueryRunner();
 
-    // connect query runner to datasource
-    await queryRunner.connect();
-    // start transaction
-    await queryRunner.startTransaction();
+    try {
+      // connect query runner to datasource
+      await queryRunner.connect();
+      // start transaction
+      await queryRunner.startTransaction();
+    } catch (error) {
+      throw new RequestTimeoutException('Could not connect to the database.');
+    }
 
     try {
       //
@@ -34,9 +42,17 @@ export class CreateManyUsersProvider {
     } catch (error) {
       // if unsuccesful, rollback transaction
       await queryRunner.rollbackTransaction();
+
+      throw new ConflictException('Could not complete transaction', {
+        description: String(error),
+      });
     } finally {
-      // release connection pool
-      await queryRunner.release();
+      try {
+        // release connection pool
+        await queryRunner.release();
+      } catch (error) {
+        throw new RequestTimeoutException('Could not release connection.');
+      }
     }
 
     return newUsers;
