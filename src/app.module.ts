260640d0b25100e41d4b054c0b59a5fce5dbd019
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-03-03 12:20:45 +0100

    feat: implement access token guard globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,10 @@ import { MetaOptionsModule } from './meta-options/meta-options.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import { PaginationModule } from './common/pagination/pagination.module';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './auth/config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 
 const ENV = process.env.NODE_ENV;
 
@@ -48,11 +52,19 @@ const ENV = process.env.NODE_ENV;
         database: configService.get('database.dbName'),
       }),
     }),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
     TagsModule,
     MetaOptionsModule,
     PaginationModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -0,0 +1,57 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    // Inject jwtService
+    private readonly jwtService: JwtService,
+
+    // Inject jwtConfiguration
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Extract request from context
+    const request = context.switchToHttp().getRequest();
+    // Extract token from headers
+    const token = this.extractRequestFromHeader(request);
+    // Validate token and return output
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+
+      console.log(payload);
+      // add a new user field to request
+      request[REQUEST_USER_KEY] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractRequestFromHeader(request: Request): string | undefined {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, token] = request.headers.authorization?.split(' ') ?? [];
+
+    return token;
+  }
+}
