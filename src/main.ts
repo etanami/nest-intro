90291fe7631779730c00c886c02a221b82a817b0
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-03-04 18:41:37 +0100

    feat: configure google authentication

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // enable cors
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
