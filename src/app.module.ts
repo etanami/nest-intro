906059183c924f91bf8a19d4567374ddd01644b6
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-03-08 13:26:31 +0100

    feat: configure nestjs mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 import { AuthenticationGuard } from './auth/guards/authentication/authentication.guard';
 import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
+import { MailModule } from './mail/mail.module';
 
 const ENV = process.env.NODE_ENV;
 
@@ -59,6 +60,7 @@ const ENV = process.env.NODE_ENV;
     TagsModule,
     MetaOptionsModule,
     PaginationModule,
+    MailModule,
   ],
   controllers: [AppController],
   providers: [
